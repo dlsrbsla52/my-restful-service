c706b12ec35472099f25b0edf7c9c97d109dd574
dlsrbsla52 <32616025+dlsrbsla52@users.noreply.github.com>
2024-10-10 15:46:17 +0900

    Swagger Documentation Swagger config 설정, 서버 헬스체크 및 모니터링을 위한 actuator 설정 변경

--- src/main/java/hig/myrestfulservice/controller/UserController.java
+++ src/main/java/hig/myrestfulservice/controller/UserController.java
@@ -3,6 +3,11 @@ package hig.myrestfulservice.controller;
 import hig.myrestfulservice.bean.User;
 import hig.myrestfulservice.dao.UserDaoService;
 import hig.myrestfulservice.exception.UserNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
@@ -18,6 +23,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "user-controller", description = "일반 사용자 서비스를 위한 컨트롤러")
 public class UserController {
 
     private final UserDaoService service;
@@ -27,8 +33,18 @@ public class UserController {
         return service.findAll();
     }
 
+    @Operation(summary = "사용자 정보 조회 API", description = "사용자 ID를 이용해서 사용자 상세 정보 조회를 합니다.")
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "USER NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+        }
+    )
     @GetMapping("/users/{id}")
-    public EntityModel<User> retrieveUsers(@PathVariable(name = "id") int id) {
+    public EntityModel<User> retrieveUsers(
+            @Parameter(description = "사용자 ID", required = true, example = "1") @PathVariable(name = "id") int id) {
 
         User user = service.findOne(id);
 
